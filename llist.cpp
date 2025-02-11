4799f9822eabe7ab8d78389ab82b7653ae35ab10
Brendan Coen <brendan@sonic.net>
2025-02-10 21:32:20 -0800

    More testing updates.

--- llist.cpp
+++ llist.cpp
@@ -4,7 +4,6 @@ struct Node {
 	int data;
 	Node *next;
 
-	// Constructor
 	Node(int val) : data(val), next(nullptr){}
 };
 
@@ -16,8 +15,6 @@ class MyLinkedList {
 
 	public:
 		MyLinkedList() : head(nullptr){}
-
-		~MyLinkedList() {} // Not sure if this is how it works!
 									 
 		void addAtHead(int val){
 
@@ -36,9 +33,8 @@ class MyLinkedList {
 			Node* newNode = new Node(val);
 
 			// first check for empty list, then traverse to tail
-			if (!head) {
-				head = newNode;
-			} else {
+			if (!head) {head = newNode;}
+			else {
 				Node* cur = head;	
 				while (cur->next) {
 					cur = cur->next;
@@ -52,7 +48,7 @@ class MyLinkedList {
 			Node* cur = head;
 			int iterCount = 0;
 
-			while(cur != nullptr){
+			while(cur->next){
 				if(iterCount == index){
 					return cur->data;
 				}
@@ -138,110 +134,108 @@ int main(){
 	// Your MyLinkedList object will be instantiated and called as such:
 	MyLinkedList* obj = new MyLinkedList();
 
-	["MyLinkedList
-	addAtHead
-	addAtTail
-	addAtTail
-	addAtTail
-	addAtTail
-	addAtTail
-	addAtTail
-	deleteAtIndex
-	addAtHead
-	addAtHead
-	get
-	addAtTail
-	addAtHead
-	get
-	addAtTail
-	addAtIndex
-	addAtTail
-	addAtHead
-	addAtHead
-	addAtHead
-	get
-	addAtIndex
-	addAtHead
-	get
-	addAtHead
-	deleteAtIndex
-	addAtHead
-	addAtTail
-	addAtTail
-	addAtIndex
-	addAtTail
-	addAtHead
-	get
-	addAtTail
-	deleteAtIndex
-	addAtIndex
-	deleteAtIndex
-	addAtHead
-	addAtTail
-	addAtHead
-	addAtHead
-	addAtTail
-	addAtTail
-	get
-	get
-	addAtHead
-	addAtTail
-	addAtTail
-	addAtTail
-	addAtIndex
-	get
-	addAtHead
-	addAtIndex
-	addAtHead
-	addAtTail
-	addAtTail
-	addAtIndex
-	deleteAtIndex
-	addAtIndex
-	addAtHead
-	addAtHead
-	deleteAtIndex
-	addAtTail
-	deleteAtIndex
-	addAtIndex
-	addAtTail
-	addAtHead
-	get
-	addAtIndex
-	addAtTail
-	addAtHead
-	addAtHead
-	addAtHead
-	addAtHead
-	addAtHead
-	addAtHead
-	deleteAtIndex
-	get
-	get
-	addAtHead
-	get
-	addAtTail
-	addAtTail
-	addAtIndex
-	addAtIndex
-	addAtHead
-	addAtTail
-	addAtTail
-	get
-	addAtIndex
-	addAtHead
-	deleteAtIndex
-	addAtTail
-	get
-	addAtHead
-	get
-	addAtHead
-	deleteAtIndex
-	get
-	addAtTail
-	addAtTail
-	[[],[38],[66],[61],[76],[26],[37],[8],[5],[4],[45],[4],[85],[37],[5],[93],[10,23],[21],[52],[15],[47],[12],[6,24],[64],[4],[31],[6],[40],[17],[15],[19,2],[11],[86],[17],[55],[15],[14,95],[22],[66],[95],[8],[47],[23],[39],[30],[27],[0],[99],[45],[4],[9,11],[6],[81],[18,32],[20],[13],[42],[37,91],[36],[10,37],[96],[57],[20],[89],[18],[41,5],[23],[75],[7],[25,51],[48],[46],[29],[85],[82],[6],[38],[14],[1],[12],[42],[42],[83],[13],[14,20],[17,34],[36],[58],[2],[38],[33,59],[37],[15],[64],[56],[0],[40],[92],[63],[35],[62],[32]]
-
+	obj->addAtHead(38);
+	obj->addAtTail(66);
+	obj->addAtTail(61);
+	obj->addAtTail(76);
+	obj->addAtTail(26);
+	obj->addAtTail(37);
+	obj->addAtTail(8);
+	obj->deleteAtIndex(5);
+	obj->addAtHead(4);
+	obj->addAtHead(45);
+	obj->get(4);
+	obj->addAtTail(85);
+	obj->addAtHead(37);
+	obj->get(5);
+	obj->addAtTail(93);
+	obj->addAtIndex(10,23);
+	obj->addAtTail(21);
+	obj->addAtHead(52);
+	obj->addAtHead(15);
+	obj->addAtHead(47);
+	obj->get(12);
+	obj->addAtIndex(6,24);
+	obj->addAtHead(64);
+	obj->get(4);
+	obj->addAtHead(31);
+	obj->deleteAtIndex(6);
+	obj->addAtHead(40);
+	obj->addAtTail(17);
+	obj->addAtTail(15);
+	obj->addAtIndex(19,2);
+	obj->addAtTail(11);
+	obj->addAtHead(86);
+	obj->get(17);
+	obj->addAtTail(55);
+	obj->deleteAtIndex(15);
+	obj->addAtIndex(14,95);
+	obj->deleteAtIndex(22);
+	obj->addAtHead(66);
+	obj->addAtTail(95);
+	obj->addAtHead(8);
+	obj->addAtHead(47);
+	obj->addAtTail(23);
+	obj->addAtTail(39);
+	obj->get(30);
+	obj->get(27);
+	obj->addAtHead(0);
+	obj->addAtTail(99);
+	obj->addAtTail(45);
+	obj->addAtTail(4);
+	obj->addAtIndex(9,11);
+	obj->get();
+	obj->addAtHead();
+	obj->addAtIndex();
+	obj->addAtHead();
+	obj->addAtTail();
+	obj->addAtTail();
+	obj->addAtIndex();
+	obj->deleteAtIndex();
+	obj->addAtIndex();
+	obj->addAtHead();
+	obj->addAtHead();
+	obj->deleteAtIndex();
+	obj->addAtTail();
+	obj->deleteAtIndex();
+	obj->addAtIndex();
+	obj->addAtTail();
+	obj->addAtHead();
+	obj->get();
+	obj->addAtIndex();
+	obj->addAtTail();
+	obj->addAtHead();
+	obj->addAtHead();
+	obj->addAtHead();
+	obj->addAtHead();
+	obj->addAtHead();
+	obj->addAtHead();
+	obj->deleteAtIndex();
+	obj->get();
+	obj->get();
+	obj->addAtHead();
+	obj->get();
+	obj->addAtTail();
+	obj->addAtTail();
+	obj->addAtIndex();
+	obj->addAtIndex();
+	obj->addAtHead();
+	obj->addAtTail();
+	obj->addAtTail();
+	obj->get();
+	obj->addAtIndex();
+	obj->addAtHead();
+	obj->deleteAtIndex();
+	obj->addAtTail();
+	obj->get();
+	obj->addAtHead();
+	obj->get();
+	obj->addAtHead();
+	obj->deleteAtIndex();
+	obj->get();
+	obj->addAtTail();
+	obj->addAtTail();
+	[6],[81],[18,32],[20],[13],[42],[37,91],[36],[10,37],[96],[57],[20],[89],[18],[41,5],[23],[75],[7],[25,51],[48],[46],[29],[85],[82],[6],[38],[14],[1],[12],[42],[42],[83],[13],[14,20],[17,34],[36],[58],[2],[38],[33,59],[37],[15],[64],[56],[0],[40],[92],[63],[35],[62],[32]]
 
 	return 0;
 }
